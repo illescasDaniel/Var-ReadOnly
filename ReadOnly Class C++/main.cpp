df96dac975949840d22df7383d34e315a77109b0
illescasDaniel <illescas.daniel@protonmail.com>
2016-10-06 11:45:31 +0200

    Initial Commit

---
+++ ReadOnly Class C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ReadOnly Class C++
+//
+//  Created by Daniel Illescas Romero on 06/10/2016.
+//  Copyright © 2016 Daniel Illescas Romero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
